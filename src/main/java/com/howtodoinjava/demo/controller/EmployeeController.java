4a161da761a0a3d394881ab64c2846474c36c225
marwen.saidi <marwen.saidi@orange.com>
2021-11-12 11:35:55 +0100

    ok

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -1,22 +1,9 @@
 package com.howtodoinjava.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.howtodoinjava.demo.model.Employee;
-
-@RestController
+// this is a Rest Controller
 public class EmployeeController {
-	
-	@RequestMapping("/")
-    public List<Employee> getEmployees() 
-    {
-		List<Employee> employeesList = new ArrayList<Employee>();
-		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
-		return employeesList;
-    }
+
+	// add 2 employee
+	// Rest Endpoint to return list of employee
 
 }
